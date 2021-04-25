626d4fb0b36f766646f414e3243353394ba0defd
kenny-io <ekeneeze3@gmail.com>
2021-04-25 22:36:12 +0400

    added shop home

---
+++ pages/index.vue
@@ -0,0 +1,29 @@
+<template>
+  <section class="text-gray-600 body-font">
+    <div class="container px-5 py-24 mx-auto">
+      <div class="flex flex-col text-center w-full mb-20">
+        <h2
+          class="text-xs text-green-500 tracking-widest font-medium title-font mb-1"
+        >
+          HEDGES STORE
+        </h2>
+        <h1
+          class="sm:text-3xl text-2xl font-medium title-font mb-4 text-gray-900"
+        >
+          Checkout all Hedges Product offerings
+        </h1>
+        <p class="lg:w-2/3 mx-auto leading-relaxed text-base">
+          Whatever cardigan tote bag tumblr hexagon brooklyn asymmetrical
+          gentrify, subway tile poke farm-to-table. Franzen you probably haven't
+          heard of them man bun deep jianbing selfies heirloom prism food truck
+          ugh squid celiac humblebrag.
+        </p>
+        <button
+          class="flex mx-auto mt-16 text-white bg-green-900 border-0 py-2 px-8 focus:outline-none hover:bg-red-900 rounded text-lg"
+        >
+          <nuxt-link :to="`/shop`">View Products </nuxt-link>
+        </button>
+      </div>
+    </div>
+  </section>
+</template>
